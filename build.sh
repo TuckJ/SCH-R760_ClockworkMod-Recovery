da7d480847ba30f72e8c28b058fe8d0043ecd17d
codeworkx <codeworkx@cyanogenmod.com>
2012-11-01 14:32:19 +0100

    update cwm to 6.0.1.5

--- build.sh
+++ build.sh
@@ -4,40 +4,43 @@
 TOOLCHAIN="/android/ics/prebuilt/linux-x86/toolchain/arm-eabi-4.4.3/bin/arm-eabi-"
 
 # don't modify
+CWM_VERSION="6.0.1.2"
 OUTDIR="out"
 INITRAMFS_SOURCE="../stage1/initramfs.list"
 INITRAMFS_ANDROID="ramdisk_android"
-MODULES=("drivers/net/wireless/bcmdhd/dhd.ko" "drivers/scsi/scsi_wait_scan.ko" "crypto/ansi_cprng.ko" "drivers/samsung/j4fs/j4fs.ko")
+INITRAMFS_RECOVERY="ramdisk_recovery"
+MODULES=("fs/cifs/cifs.ko" "drivers/net/wireless/bcmdhd/dhd.ko" "drivers/scsi/scsi_wait_scan.ko" "crypto/ansi_cprng.ko" "drivers/samsung/j4fs/j4fs.ko")
 
 case "$1" in
 	clean)
         cd kernel
-        make mrproper ARCH=arm CROSS_COMPILE=$TOOLCHAIN
+        make mrproper ARCH=arm CROSS_COMPILE=${TOOLCHAIN}
         cd ..
 
         rm stage1/files/boot.cpio
         rm stage1/files/recovery.cpio
-        rm out/zImage
+        rm -rf ${OUTDIR}
 		;;
 	*)
+		mkdir -p ${OUTDIR}
         cd kernel
-        make clockworkmod_i9100g_defconfig ARCH=arm CROSS_COMPILE=$TOOLCHAIN
+        make clockworkmod_i9100g_defconfig ARCH=arm CROSS_COMPILE=${TOOLCHAIN}
 
         # build modules first to include them into android ramdisk
-        make -j8 ARCH=arm CROSS_COMPILE=$TOOLCHAIN modules
+        make -j8 ARCH=arm CROSS_COMPILE=${TOOLCHAIN} modules
        
         for module in "${MODULES[@]}" ; do
-            cp "$module" ../ramdisk_android/lib/modules/
+            cp "${module}" ../${INITRAMFS_ANDROID}/lib/modules/
         done
         cd ..
 
         # create the android ramdisk
-        cd ramdisk_android
+        cd ${INITRAMFS_ANDROID}
         find . | cpio -o -H newc > ../stage1/files/boot.cpio
         cd ..
 
         # create the recovery ramdisk
-        cd ramdisk_recovery
+        cd ${INITRAMFS_RECOVERY}
         find . | cpio -o -H newc > ../stage1/files/recovery.cpio
         cd ..
         
@@ -45,6 +48,8 @@ case "$1" in
         cd kernel
         make -j8 ARCH=arm CROSS_COMPILE=${TOOLCHAIN} CONFIG_INITRAMFS_SOURCE=${INITRAMFS_SOURCE} zImage
         cp arch/arm/boot/zImage ../${OUTDIR}
+        cd ../${OUTDIR}
+        tar -cf GT-I9100G_ICS_ClockworkMod-Recovery_${CWM_VERSION}.tar zImage
         cd ..
 	    ;;
 esac
