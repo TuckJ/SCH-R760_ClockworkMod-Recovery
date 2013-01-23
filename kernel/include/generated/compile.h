67a34ba180ff9b246edf8aae791b729af4ad9d2d
Joe <joe@Desktop-Ubuntu.(none)>
2013-01-22 20:24:18 -0600

    Adding R760 stuff.

--- kernel/include/generated/compile.h
+++ kernel/include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 3 */
+/* This file is auto generated, version 5 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#3 SMP PREEMPT Tue Jan 22 19:16:34 CST 2013"
+#define UTS_VERSION "#5 SMP PREEMPT Tue Jan 22 20:13:15 CST 2013"
 #define LINUX_COMPILE_BY "joe"
 #define LINUX_COMPILE_HOST "Desktop-Ubuntu"
 #define LINUX_COMPILER "gcc version 4.4.3 (GCC) "
