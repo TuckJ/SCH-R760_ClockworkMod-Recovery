56024f09e19477e89d4330350d3b7d2ab431a160
Joe <joe@Desktop-Ubuntu.(none)>
2013-01-22 21:19:40 -0600

    Working on fixing recovery.

--- kernel/include/generated/compile.h
+++ kernel/include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 5 */
+/* This file is auto generated, version 11 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#5 SMP PREEMPT Tue Jan 22 20:13:15 CST 2013"
+#define UTS_VERSION "#11 SMP PREEMPT Tue Jan 22 21:18:25 CST 2013"
 #define LINUX_COMPILE_BY "joe"
 #define LINUX_COMPILE_HOST "Desktop-Ubuntu"
 #define LINUX_COMPILER "gcc version 4.4.3 (GCC) "
